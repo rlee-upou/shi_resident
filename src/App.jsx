f33e6224b1682235a9c7a2bad9a4dba39a6f6f8c
rlee-upou <rlee@up.edu.ph>
2026-03-26 10:07:11 +0800

    updated apk url

--- src/App.jsx
+++ src/App.jsx
@@ -245,7 +245,7 @@ export default function ResidentApp() {
 
   const handleDownloadAccept = () => {
     // Replace this URL with your actual public Google Cloud Storage link
-    const apkUrl = 'https://drive.google.com/file/d/1B4HQKSUg9dJijgaVkbBImStKzs1mPfba/view?usp=sharing';
+    const apkUrl = 'https://drive.google.com/file/d/1YeLfGbZwNSk20P6uosR8qxQB34Q7xS5E/view?usp=sharing';
     
     // Create an invisible anchor tag to trigger the browser's download manager
     const link = document.createElement('a');
